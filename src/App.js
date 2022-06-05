cefcf42b44ee907d5575816f9b02b67d3e3fe834
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-06-05 20:43:48 +1000

    Basic transitions.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { useRef } from 'react';
+
 import './App.css'
 
 function App() {
-  let text = "Hello there";
+  const parallaxRef = useRef();
+
+  const scroll = (to) => {
+    if (parallaxRef.current) {
+      parallaxRef.current.scrollTo(to);
+    }
+  }
 
   return (
-    <div className="App">
-      <p className='H1'>{text}</p>
-      <p className='H2'>{text}</p>
-      <p className='H3'>{text}</p>
-    </div>
+    <Parallax horizontal ref={parallaxRef} pages={3} style={{ top: '0', left: '0' }}>
+      <ParallaxLayer offset={0}>
+        <p className='H1'>Hero</p>
+        <button onClick={() => scroll(1)}>click to scroll</button>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1}>
+        <p className='H1'>Projects</p>
+        <button onClick={() => scroll(2)}>click to scroll</button>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={2}>
+        <p className='H1'>About</p>
+        <button onClick={() => scroll(0)}>click to scroll</button>
+      </ParallaxLayer>
+    </Parallax>
   );
 }
 
