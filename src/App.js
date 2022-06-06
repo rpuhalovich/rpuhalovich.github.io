3194a21f974f335c060ca2e8dbd54c5d04c65879
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-06-06 16:05:43 +1000

    Hid scrollbar.

--- src/App.js
+++ src/App.js
@@ -18,24 +18,40 @@ function App() {
 
   return (
     <div className='hidesb1 hidesb2'>
-      <Parallax enabled={false} horizontal ref={parallaxRef} pages={3} style={{ top: '0', left: '0' }}>
+      {/* // TODO: Implement side scroll snapping. */}
+      <Parallax horizontal ref={parallaxRef} pages={3} style={{
+        top: '0',
+        left: '0',
+        scrollSnapType: 'x mandatory',
+        overflowX: 'scroll'  
+      }}>
+        {/* background */}
+        <ParallaxLayer speed={1} style={{ scrollSnapAlign: 'start' }}>
+          <img alt='' src={'../res/layer1.svg'} />
+        </ParallaxLayer>
+
         <ParallaxLayer offset={0}>
           <Hero />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1}>
+        <ParallaxLayer offset={1} style={{ scrollSnapAlign: 'start' }}>
           <h2>Projects</h2>
+          <p className='Monospace'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Elit, faucibus eu ultrices morbi felis sagittis aliquam vitae amet.
+            Id congue aliquet porta gravida fames porta amet.
+          </p>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={2}>
+        <ParallaxLayer offset={2} style={{ scrollSnapAlign: 'start' }}>
           <h3>About</h3>
         </ParallaxLayer>
 
         {/* Lower navigation buttons. */}
         <ParallaxLayer className='bottom-buttons' sticky={{ start: 0, end: 2 }}>
-          <NavButton onClick={() => scroll(0)}/>
-          <NavButton onClick={() => scroll(1)}/>
-          <NavButton onClick={() => scroll(2)}/>
+          <NavButton onClick={() => scroll(0)} />
+          <NavButton onClick={() => scroll(1)} />
+          <NavButton onClick={() => scroll(2)} />
         </ParallaxLayer>
       </Parallax>
 
