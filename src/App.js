974a6d941005acfabfd34108fa1db3a77802996b
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-06-21 02:06:46 +1000

    Now can press buttons to scroll.

--- src/App.js
+++ src/App.js
@@ -10,32 +10,39 @@ import Projects from "./views/Projects";
 import "./App.css";
 
 function App() {
+  const heroSection = useRef(null);
+  const projectsSection = useRef(null);
+  const aboutSection = useRef(null);
+
+  const scrollToSection = (ref) => {
+    document.getElementById("parent").scrollTo({ left: ref.current.offsetLeft, behavior: "smooth" });
+  };
+
   return (
     <div className="hidesb1 hidesb2">
-      {/* TODO: Implement side scroll snapping. */}
-      <div className="parent">
-
-        <section>
+      <div id="parent" className="parent">
+        <section ref={heroSection}>
           <SideLabel text="Hero" />
           <Hero />
         </section>
 
-        <section>
+        <section ref={projectsSection}>
           <SideLabel text="Projects" />
           <Projects />
+
+          {/* Lower navigation buttons. */}
+          <div className="bottom-buttons">
+            <NavButton onClick={() => scrollToSection(heroSection)} />
+            <NavButton onClick={() => scrollToSection(projectsSection)} />
+            <NavButton onClick={() => scrollToSection(aboutSection)} />
+          </div>
         </section>
 
-        <section>
+        <section ref={aboutSection}>
           <SideLabel text="About" />
           <About />
         </section>
 
-        {/* Lower navigation buttons. */}
-        {/* <div className="bottom-buttons">
-          <NavButton onClick={() => scroll(0)} />
-          <NavButton onClick={() => scroll(1)} />
-          <NavButton onClick={() => scroll(2)} />
-        </div> */}
       </div>
     </div>
   );
