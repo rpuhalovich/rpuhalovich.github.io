76881196e0064d0c039c925c3b7b6a0f896782cb
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-06 16:12:25 +1100

    Done the rebase I guess.

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ import RPCard from "./views/components/RPCard";
 
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <>
       <div className="appGridContainer">
-        <section className="view1 viewBounds appGridChild">
-          <div className="headerContainer">
-            <div className="header">
-              {/* TODO: use media queries to make mobile friendly */}
-              <p className="h3">
-                <span className="h5">#</span>Ryan Puhalovich
-              </p>
-              {/* <p className="h5"><span className="h6">##</span>Algorithms Are Fun</p> */}
-            </div>
-          </div>
+        <section className="view1">
+          <p className="h3">
+            <span className="h5">#</span>Ryan Puhalovich
+          </p>
         </section>
+      </div>
 
-        <section className="view2 viewBounds appGridChild">
+      {/* <section className="view2 viewBounds appGridChild">
           <div className="card">
             <RPCard>
               <p>Hey there from inside a p tag.</p>
@@ -31,10 +26,7 @@ function App() {
 
         <section className="view3 viewBounds appGridChild">
           <RPButton onClick={() => window.open("https://github.com/rpuhalovich")}>Github</RPButton>
-        </section>
-      </div>
+        </section> */}
     </>
   );
 }
-
-export default App;
