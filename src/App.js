e5280af19bbef1dedafdb911713493aee09827da
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-18 21:53:40 +1100

    changes

--- src/App.js
+++ src/App.js
@@ -11,36 +11,50 @@ export default function App() {
       <div className="appGridContainer">
         <section className="view1">
           <div className="headerContainer">
-            <div className="header">
-              <p className="h3">
-                <span className="h5">#</span>Ryan Puhalovich
-              </p>
-            </div>
+            <p className="h3 header">
+              <span className="h5">#</span>Ryan Puhalovich
+            </p>
+            <p className="subHeader">
+              <span className="">#</span> Software Dev
+            </p>
           </div>
         </section>
 
         <section className="view2">
           <div>
-            <RPCard>
+            <RPCard className="cardGap">
               <p>Hey there from inside a p tag.</p>
             </RPCard>
-            <div className="bottomCard">
-              <RPCard className="cardGap">
-                <p>Hey there from inside another p tag.</p>
-              </RPCard>
-            </div>
+            <RPCard className="cardGap bottomCard">
+              <p>Hey there from inside another p tag.</p>
+            </RPCard>
           </div>
         </section>
 
-        <section className="view3">
-          <RPButton onClick={() => window.open("https://github.com/rpuhalovich")}>Github</RPButton>
-          <RPButton className="buttonGap" onClick={() => window.open("https://github.com/rpuhalovich")}>
-            Linkedin
-          </RPButton>
-          <p>This is a sample about.</p>
-          <RPCard>
-            <p>This is a sample about.</p>
-          </RPCard>
+        <section className="view3 aboutContainer">
+          <div className="about">
+            <div className="aboutContentContainer">
+              <div>
+                <div className="tmpimg" />
+              </div>
+              <div>
+                <p>
+                  Aliqua commodo deserunt tempor nulla non ut proident consequat esse laborum nostrud. Dolore laboris
+                  consequat enim commodo ipsum minim. Amet magna eu cupidatat sint in veniam Lorem ex. Fugiat ea ea sit
+                  sint anim voluptate aliquip officia do. Do incididunt excepteur nulla ipsum deserunt laboris dolore
+                  officia sit. Nostrud commodo excepteur anim ea ex occaecat laboris non nisi adipisicing nulla.
+                </p>
+
+                <RPButton onClick={() => window.open("https://github.com/rpuhalovich")}>GitHub</RPButton>
+                <RPButton className="buttonGap" onClick={() => window.open("https://github.com/rpuhalovich")}>
+                  Linkedin
+                </RPButton>
+                {/* <RPButton className="buttonGap" onClick={() => window.open("mailto:reanpuhalovich@gmail.com")}>
+                  Email
+                </RPButton> */}
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </>
