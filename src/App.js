4eab42af7de72d3c95ae04390152f4334d66b0c4
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-25 22:02:41 +1100

    Layout looks good.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import RPButton from "./views/components/RPButton";
 import RPCard from "./views/components/RPCard";
 import Hero from "./views/Hero";
+import About from "./views/About";
 
 import "./App.css";
 
@@ -12,7 +12,9 @@ export default function App() {
       <div className="appGridContainer">
         <section className="view1">
           <div className="headerContainer">
-            <Hero />
+            <div className="header">
+              <Hero />
+            </div>
           </div>
         </section>
 
@@ -25,26 +27,7 @@ export default function App() {
         </section>
 
         <section className="view3 aboutContainer">
-          <div className="about">
-            <div className="aboutContentContainer">
-              <div>
-                <div className="tmpimg" />
-              </div>
-              <div>
-                <p>
-                  Aliqua commodo deserunt tempor nulla non ut proident consequat esse laborum nostrud. Dolore laboris
-                  consequat enim commodo ipsum minim. Amet magna eu cupidatat sint in veniam Lorem ex. Fugiat ea ea sit
-                  sint anim voluptate aliquip officia do. Do incididunt excepteur nulla ipsum deserunt laboris dolore
-                  officia sit. Nostrud commodo excepteur anim ea ex occaecat laboris non nisi adipisicing nulla.
-                </p>
-
-                <RPButton onClick={() => window.open("https://github.com/rpuhalovich")}>GitHub</RPButton>
-                <RPButton className="buttonGap" onClick={() => window.open("https://github.com/rpuhalovich")}>
-                  Linkedin
-                </RPButton>
-              </div>
-            </div>
-          </div>
+          <About />
         </section>
       </div>
     </>
--- src/views/About.js
+++ src/views/About.js
@@ -1,12 +1,33 @@
 import React from "react";
 
+import RPButton from "./components/RPButton";
+
 import "./About.css";
 
 function About() {
   return (
-    <div className="title">
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="about">
+        <div className="aboutContainer flexDirection">
+          <div>
+            <div className="tmpimg imgRightGap" />
+          </div>
+          <div className="centerText">
+            <p>
+              Aliqua commodo deserunt tempor nulla non ut proident consequat esse laborum nostrud. Dolore laboris
+              consequat enim commodo ipsum minim. Amet magna eu cupidatat sint in veniam Lorem ex. Fugiat ea ea sit sint
+              anim voluptate aliquip officia do. Do incididunt excepteur nulla ipsum deserunt laboris dolore officia
+              sit. Nostrud commodo excepteur anim ea ex occaecat laboris non nisi adipisicing nulla.
+            </p>
+
+            <RPButton onClick={() => window.open("https://github.com/rpuhalovich")}>GitHub</RPButton>
+            <RPButton className="buttonGap" onClick={() => window.open("https://www.linkedin.com/in/ryan-puhalovich/")}>
+              Linkedin
+            </RPButton>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
