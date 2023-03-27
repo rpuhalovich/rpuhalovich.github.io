62d4be06d764654a7cf7e66352c319d059643ef3
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-27 16:44:43 +1100

    Added more project.

--- src/App.js
+++ src/App.js
@@ -45,10 +45,14 @@ export default function App() {
           </section>
 
           <section className="view2">
+            <p className="subHeader">
+              <span className="smolSpace">##</span>Projects
+            </p>
+
             <div className="cardsContainer">
               <RPCard className="oglp">
                 <div
-                  className="oglpContainer"
+                  className="cardContainer"
                   onClick={() => window.open("https://github.com/rpuhalovich/OpenGLPathfinder")}
                 >
                   <img
@@ -61,7 +65,49 @@ export default function App() {
                   <div>
                     <h3>OpenGL Pathfinder</h3>
                     <p>A BFS shortest path algorithm, written in C++ using raw OpenGL.</p>
-                    <p className="Monospace">C++;CMake;OpenGL</p>
+                    <p className="Monospace">C++;CMake;OpenGL;DSA</p>
+                  </div>
+                </div>
+              </RPCard>
+            </div>
+
+            <div className="cardsContainer marginTop2em">
+              <RPCard className="loap">
+                <div
+                  className="cardContainer"
+                  onClick={() => window.open("https://github.com/rpuhalovich/life_of_a_programmer.exe")}
+                >
+                  <img src={require("./res/loap.png")} alt="life_of_a_programmer.exe" width="1920" height="1080" />
+                  <div>
+                    <h3>life_of_a_programmer.exe</h3>
+                    <p>A metaphorical game about running through code to catch the bug at the end of the level.</p>
+                    <p className="Monospace">C#;Unity</p>
+                  </div>
+                </div>
+              </RPCard>
+            </div>
+
+            <div className="cardsContainer marginTop2em">
+              <RPCard className="vpeq">
+                <div className="cardContainer" onClick={() => window.open("https://github.com/rpuhalovich/VPEQ")}>
+                  <img src={require("./res/vpeq.png")} alt="VPEQ" width="640" height="650" />
+                  <div>
+                    <h3>VPEQ</h3>
+                    <p>A parametric equalizer made using the VSTSDK framework.</p>
+                    <p className="Monospace">C++;CMake;VSTSDK;DSP</p>
+                  </div>
+                </div>
+              </RPCard>
+            </div>
+
+            <div className="cardsContainer marginTop2em">
+              <RPCard className="rayTracer">
+                <div className="cardContainer" onClick={() => window.open("https://github.com/rpuhalovich/VPEQ")}>
+                  <img src={require("./res/raytracer.png")} alt="C# Ray Tracer" width="640" height="650" />
+                  <div>
+                    <h3>C# Ray Tracer</h3>
+                    <p>A ray tracer written in C#.</p>
+                    <p className="Monospace">C#;Visual Studio</p>
                   </div>
                 </div>
               </RPCard>
