64f4ce6c817778436696187405a9c2478f0cf8b4
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-28 14:16:43 +1100

    First card laid out.

--- src/App.js
+++ src/App.js
@@ -27,47 +27,47 @@ export default function App() {
   return (
     <>
       <div onScroll={handleScroll}>
-        <div className="appGridContainer">
-          <div className="downArrowContainer" onClick={() => window.scrollByPages(1)}>
+        <div className="appContainer">
+          {/* <div className="downArrowContainer" onClick={() => window.scrollByPages(1)}>
             {(window.screenX >= 750) &
               (showArrow ? (
                 <ExpandMoreIcon className="downArrow fadeInArrow" />
               ) : (
                 <ExpandMoreIcon className="downArrow fadeOutArrow" />
               ))}
-          </div>
+          </div> */}
 
           <section className="view1">
             <div className="headerContainer">
-              <div className="header">
+              <div>
                 <Hero />
               </div>
             </div>
           </section>
 
-          <section className="view2 view2Columns">
-            <div className="projectsContainer">
-              <div className="projects">
-                <p className="subHeader">
-                  <span className="smolSpace">##</span>Projects
-                </p>
+          <section className="view2">
+            <div className="projectsContainer" style={{ width: "min(1000px, 80%)" }}>
+              <div className="titleContainer">
+                <div className="title">
+                  <p className="subHeader">
+                    <span className="smolSpace">##</span>Projects
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <div className="cardsContainer">
-              <RPCard className="oglp">
-                <div
-                  className="cardContainerHorizontal flexDirection"
-                  onClick={() => window.open("https://github.com/rpuhalovich/OpenGLPathfinder")}
-                >
+              <RPCard
+                className="cardContainer"
+                onClick={() => window.open("https://github.com/rpuhalovich/OpenGLPathfinder")}
+              >
+                <div className="cardContentContainer">
                   <img
-                    className="cardImage cardImageSizeHorizontal"
+                    className="cardImage"
                     src={require("./res/openGLPathfinder.png")}
                     alt="OpenGL Pathfinder"
                     width="1291"
                     height="715"
                   />
-                  <div>
+                  <div style={{ margin: "0px" }}>
                     <h3>OpenGL Pathfinder</h3>
                     <p>A BFS shortest path algorithm, written in C++ using raw OpenGL.</p>
                     <p className="Monospace">C++;CMake;OpenGL;DSA</p>
@@ -75,83 +75,9 @@ export default function App() {
                 </div>
               </RPCard>
             </div>
-
-            <div className="cardsContainer marginTop2em">
-              <RPCard className="loap">
-                <div
-                  className="cardContainerHorizontal flexDirection"
-                  onClick={() => window.open("https://github.com/rpuhalovich/life_of_a_programmer.exe")}
-                >
-                  <img
-                    className="cardImage cardImageSizeHorizontal"
-                    src={require("./res/loap.png")}
-                    alt="life_of_a_programmer.exe"
-                    width="1920"
-                    height="1080"
-                  />
-                  <div>
-                    <h3>life_of_a_programmer.exe</h3>
-                    <p>A metaphorical game about running through code to catch the bug at the end of the level.</p>
-                    <p className="Monospace">C#;Unity</p>
-                  </div>
-                </div>
-              </RPCard>
-            </div>
-
-            <div className="cardsContainer marginTop2em">
-              <RPCard className="vpeq">
-                <div
-                  className="cardContainerVertical flexDirection"
-                  onClick={() => window.open("https://github.com/rpuhalovich/VPEQ")}
-                >
-                  <img
-                    className="cardImage cardImageSizeVertical"
-                    src={require("./res/vpeq.png")}
-                    alt="VPEQ"
-                    width="640"
-                    height="650"
-                  />
-                  <div>
-                    <h3>VPEQ</h3>
-                    <p>
-                      A parametric equalizer made using the VSTSDK framework. Contains implementations of various DSP
-                      concepts.
-                    </p>
-                    <p className="Monospace">C++;CMake;VSTSDK;DSP</p>
-                  </div>
-                </div>
-              </RPCard>
-            </div>
-
-            <div className="cardsContainer marginTop2em">
-              <RPCard className="rayTracer">
-                <div
-                  className="cardContainerVertical flexDirection"
-                  onClick={() => window.open("https://github.com/rpuhalovich/CSharpRayTracer")}
-                >
-                  <img
-                    className="cardImage cardImageSizeVertical"
-                    src={require("./res/raytracer.png")}
-                    alt="C# Ray Tracer"
-                    width="640"
-                    height="650"
-                  />
-                  <div>
-                    <h3>C# Ray Tracer</h3>
-                    <p>
-                      A ray tracer written in C#. Contains implementations of depth of field, reflection, refraction and
-                      the Fresnel effect.
-                    </p>
-                    <p className="Monospace">C#;Visual Studio</p>
-                  </div>
-                </div>
-              </RPCard>
-            </div>
           </section>
 
-          <section className="view3 view3Columns">
-            <About />
-          </section>
+          <section className="view3">{/* <About /> */}</section>
         </div>
       </div>
     </>
