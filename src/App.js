3e2ebbf5b32d83fd396e9366229121d7266d98a7
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-28 16:51:46 +1100

    Now fading and have projects sticking through.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import React from "react";
 
 import Hero from "./views/Hero";
 import About from "./views/About";
@@ -8,51 +7,24 @@ import "./App.css";
 import Projects from "./views/Projects";
 
 export default function App() {
-  const [showArrow, setShowArrow] = useState(true);
-
-  const handleScroll = (e) => {
-    if (window.screenY !== 0) {
-      setShowArrow(false);
-    }
-  };
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <>
-      <div onScroll={handleScroll}>
-        <div className="appContainer">
-          {/* <div className="downArrowContainer" onClick={() => window.scrollByPages(1)}>
-            {(window.screenX >= 750) &
-              (showArrow ? (
-                <ExpandMoreIcon className="downArrow fadeInArrow" />
-              ) : (
-                <ExpandMoreIcon className="downArrow fadeOutArrow" />
-              ))}
-          </div> */}
-
-          <section className="view1">
-            <div className="headerContainer">
-              <div>
-                <Hero />
-              </div>
+      <div className="appContainer">
+        <section className="view1">
+          <div className="headerContainer">
+            <div>
+              <Hero />
             </div>
-          </section>
+          </div>
+        </section>
 
-          <section className="view2">
-            <Projects />
-          </section>
+        <section className="view2">
+          <Projects />
+        </section>
 
-          <section className="view3">
-            <About />
-          </section>
-        </div>
+        <section className="view3">
+          <About />
+        </section>
       </div>
     </>
   );
