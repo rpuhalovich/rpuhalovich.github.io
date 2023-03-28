bda595951cd18e98d6a7379df1bba29671629d13
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-28 11:01:03 +1100

    Now projects is aligned properly.

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
   return (
     <>
       <div onScroll={handleScroll}>
-        <div className="appGridContainer wave">
+        <div className="appGridContainer">
           <div className="downArrowContainer" onClick={() => window.scrollByPages(1)}>
             {showArrow ? (
               <ExpandMoreIcon className="downArrow fadeInArrow" />
@@ -44,21 +44,23 @@ export default function App() {
             </div>
           </section>
 
-          <section className="view2">
+          <section className="view2 view2Columns">
             <div className="projectsContainer">
-              <p className="subHeader">
-                <span className="smolSpace">##</span>Projects
-              </p>
+              <div className="projects">
+                <p className="subHeader">
+                  <span className="smolSpace">##</span>Projects
+                </p>
+              </div>
             </div>
 
             <div className="cardsContainer">
               <RPCard className="oglp">
                 <div
-                  className="cardContainer flexDirection"
+                  className="cardContainerHorizontal flexDirection"
                   onClick={() => window.open("https://github.com/rpuhalovich/OpenGLPathfinder")}
                 >
                   <img
-                    className="cardImage cardImageSize"
+                    className="cardImage cardImageSizeHorizontal"
                     src={require("./res/openGLPathfinder.png")}
                     alt="OpenGL Pathfinder"
                     width="1291"
@@ -76,11 +78,11 @@ export default function App() {
             <div className="cardsContainer marginTop2em">
               <RPCard className="loap">
                 <div
-                  className="cardContainer"
+                  className="cardContainerHorizontal flexDirection"
                   onClick={() => window.open("https://github.com/rpuhalovich/life_of_a_programmer.exe")}
                 >
                   <img
-                    className="cardImage cardImageSize"
+                    className="cardImage cardImageSizeHorizontal"
                     src={require("./res/loap.png")}
                     alt="life_of_a_programmer.exe"
                     width="1920"
@@ -97,9 +99,12 @@ export default function App() {
 
             <div className="cardsContainer marginTop2em">
               <RPCard className="vpeq">
-                <div className="cardContainer" onClick={() => window.open("https://github.com/rpuhalovich/VPEQ")}>
+                <div
+                  className="cardContainerVertical flexDirection"
+                  onClick={() => window.open("https://github.com/rpuhalovich/VPEQ")}
+                >
                   <img
-                    className="cardImage cardImageSize"
+                    className="cardImage cardImageSizeVertical"
                     src={require("./res/vpeq.png")}
                     alt="VPEQ"
                     width="640"
@@ -107,7 +112,10 @@ export default function App() {
                   />
                   <div>
                     <h3>VPEQ</h3>
-                    <p>A parametric equalizer made using the VSTSDK framework.</p>
+                    <p>
+                      A parametric equalizer made using the VSTSDK framework. Contains implementations of various DSP
+                      concepts.
+                    </p>
                     <p className="Monospace">C++;CMake;VSTSDK;DSP</p>
                   </div>
                 </div>
@@ -116,9 +124,12 @@ export default function App() {
 
             <div className="cardsContainer marginTop2em">
               <RPCard className="rayTracer">
-                <div className="cardContainer" onClick={() => window.open("https://github.com/rpuhalovich/VPEQ")}>
+                <div
+                  className="cardContainerVertical flexDirection"
+                  onClick={() => window.open("https://github.com/rpuhalovich/CSharpRayTracer")}
+                >
                   <img
-                    className="cardImage cardImageSize"
+                    className="cardImage cardImageSizeVertical"
                     src={require("./res/raytracer.png")}
                     alt="C# Ray Tracer"
                     width="640"
@@ -126,7 +137,10 @@ export default function App() {
                   />
                   <div>
                     <h3>C# Ray Tracer</h3>
-                    <p>A ray tracer written in C#.</p>
+                    <p>
+                      A ray tracer written in C#. Contains implementations of depth of field, reflection, refraction and
+                      the Fresnel effect.
+                    </p>
                     <p className="Monospace">C#;Visual Studio</p>
                   </div>
                 </div>
@@ -134,7 +148,7 @@ export default function App() {
             </div>
           </section>
 
-          <section className="view3">
+          <section className="view3 view3Columns">
             <About />
           </section>
         </div>
