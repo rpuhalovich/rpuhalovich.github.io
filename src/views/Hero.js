e76300953cff239619cfb88c2d8cd2e37ab672ba
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-02-15 18:37:54 +1100

    Update sub header

--- src/views/Hero.js
+++ src/views/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <>
       <p className="title header">Ryan Puhalovich</p>
-      <p className="subHeader">Software Engineer at Bilue</p>
+      <p className="subHeader">Software Engineer</p>
     </>
   );
 }
