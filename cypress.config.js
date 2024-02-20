fcc3f8a93ddade2fe121e3bc61f0202ebb1d1e8f
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-02-20 16:17:17 +1100

    Update headless viewport size and font loading

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.name === "electron" && browser.isHeadless) {
+          launchOptions.preferences.width = 1920;
+          launchOptions.preferences.height = 1080;
+        }
+        return launchOptions;
+      });
     },
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,25 +1,37 @@
-describe("spec", () => {
-  it("screenshots", () => {
-    Cypress.Screenshot.defaults({ capture: "fullPage", overwrite: true });
+describe(
+  "spec",
+  {
+    viewportHeight: 1920,
+    viewportWidth: 1080,
+  },
+  () => {
+    it("screenshots", () => {
+      // loading stuff
+      Cypress.Screenshot.defaults({ capture: "fullPage", overwrite: true });
+      cy.visit("http://localhost:3000");
 
-    cy.visit("http://localhost:3000");
+      // font loading (screenshots can happen before fonts are loaded)
+      cy.document().its("fonts.status").should("equal", "loaded");
+      cy.wait(1000); // eslint-disable-line
 
-    // get rid of css fade in
-    cy.get("#appContainer").invoke("css", "opacity", "1");
+      // get rid of css fade in
+      cy.get("#appContainer").invoke("css", "opacity", "1");
 
-    cy.viewport(1920, 1080).wait(100); // eslint-disable-line
-    cy.screenshot("1920x1080");
+      // screenshots
+      cy.viewport(1920, 1080).wait(100); // eslint-disable-line
+      cy.screenshot("1920x1080");
 
-    cy.viewport(1550, 1080).wait(100); // eslint-disable-line
-    cy.screenshot("1550x1080");
+      cy.viewport(1550, 1080).wait(100); // eslint-disable-line
+      cy.screenshot("1550x1080");
 
-    cy.viewport(1040, 1080).wait(100); // eslint-disable-line
-    cy.screenshot("1040x1080");
+      cy.viewport(1040, 1080).wait(100); // eslint-disable-line
+      cy.screenshot("1040x1080");
 
-    cy.viewport(740, 1080).wait(100); // eslint-disable-line
-    cy.screenshot("740x1080");
+      cy.viewport(740, 1080).wait(100); // eslint-disable-line
+      cy.screenshot("740x1080");
 
-    cy.viewport(420, 1080).wait(100); // eslint-disable-line
-    cy.screenshot("420x1080");
-  });
-});
+      cy.viewport(420, 1080).wait(100); // eslint-disable-line
+      cy.screenshot("420x1080");
+    });
+  }
+);
